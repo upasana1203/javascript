a70936d11652f492f9962fdd4a35e01aab5cd60c
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2024-11-04 16:14:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33";
+// let score = "33abc"; 
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = "upasana";
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// let isLoggedIn = 0;
+// let isLoggedIn = "upasana";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0=> false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 56;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+ 
