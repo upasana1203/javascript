393f4eed95b3758124868702d4d5cb523c6906fb
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2024-11-04 15:54:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all js cose as newer version
+
+// alert(3 + 3)   //noy used becuse we are using node not browser
+
+// data types
+let age = 18;
+let isLoggedIn = false;
+let state;  //datatype = undefined
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+// typeof
+console.log(typeof age);
+console.log(typeof null);   //=> object
+console.log(typeof undefined);   //=>undefined
+
