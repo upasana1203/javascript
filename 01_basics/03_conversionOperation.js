ea99d81d778327f99fb53060a32a23fb0198674b
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2024-11-13 21:05:05 +0000

    comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,11 +29,65 @@
 // "" => false
 // "hitesh" => true
 
-let someNumber = 56;
+// let someNumber = 56;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************OPERATIONS***************
+// let value = 3
+// let  negValue = -value
+// console.log(negValue);
+
+// let str1 = "Upasana"
+// let str2 = " Sharma"
+// let str3 = str1 + str2
+// console.log(str3)
 
 
- 
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 1 + 2);           // from the point we have string from that point it will only cancatinate the values
+// console.log( 2 + 1 + 3 + "1" + 1 + 2); 
+// console.log(1 + 1 + "2");           // whereas the values before the string  , all values get sum first and then the string value will be printed
+
+// // confusion conversions
+// console.log(+true);
+// // console.log(true+);         //gives error
+// console.log(+"");
+
+// let gameCounter = 100
+// gameCounter++
+// // console.log(gameCounter);
+// console.log(gameCounter++)
+// console.log(gameCounter)
+
+
+//*******Comparision******* *//
+// console.log(2 > 4);
+// console.log(2 < 4);
+// console.log(2 == 4);
+// console.log(2 != 4);
+// console.log(3 >> 4);
+// console.log(3 << 4);
+
+// always try compare same data types  
+// console.log("2" > 1);      //string already converts the 2 to the integer 2
+// console.log("02" > 1);
+
+
+// always try to avoid such type comparisions
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+
+
+// strict check(===)
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
