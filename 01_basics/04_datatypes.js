752a61ab230ebefeccf40d6a968e07c5309d08e4
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2024-11-15 19:24:20 +0000

    stack and heap understanding

--- 01_basics/04_datatypes.js
+++ 01_basics/04_datatypes.js
@@ -4,33 +4,33 @@
 //7 - string , number , boolean , null , undefined , BigInt , symbol
 
 //SYMBOL
-const id = Symbol('123')
-const idStudent = Symbol('123')
+// const id = Symbol('123')
+// const idStudent = Symbol('123')
 
-console.log(id === idStudent);
+// console.log(id === idStudent);
 
 //BIGINT 
-const bigNumber = 12345678956789n
+// const bigNumber = 12345678956789n
 
 
 //2 - refernece(non-primitive)
 // 3 - array , object , functions
 
 //arrays 
-const heroes = ["shaktiman" , "naagraj" , "doga"]
+// const heroes = ["shaktiman" , "naagraj" , "doga"]
 
 //objects 
-let myobj = {
-    name : "Upasana",
-    age : 20,
-}
+// let myobj = {
+//     name : "Upasana",
+//     age : 20,
+// }
 
 // does js is statically typed or dynamically typed
 
-let myFunction = function(){
-    console.log("hello world");
+// let myFunction = function(){
+//     console.log("hello world");
     
-}
+// }
 //Return type of variables in JavaScript
 // 1) Primitive Datatypes
 // Number => number
@@ -45,3 +45,34 @@ let myFunction = function(){
 // Arrays  =>  object
 // Function  =>  function
 // Object  =>  object
+
+
+// ***************************************************************************
+
+// stack(primitive datatypes) - get copy of variable
+
+// heap(non - primitive datatypes) - get reference , so whatever changes you do are made in original value
+
+// stack
+let myYoutubeName = "upasanasharma"
+let anotherName = myYoutubeName
+anotherName = "sharma12"
+
+console.log(anotherName);
+console.log(myYoutubeName);  //change occurs in copy
+
+
+// heap
+let userOne = {
+    email : "sharma@gmail.com",
+    upi : "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "upasana@123"
+
+//takes value from reference so change will occur in original as well
+console.log(userTwo.email);
+console.log(userOne.email);
+
