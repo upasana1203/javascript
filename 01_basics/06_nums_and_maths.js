838903211fbd400091dc672a4f36964facb31827
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2024-11-16 15:30:21 +0000

    NUM AND MATHS

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,44 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumeber = 23.8999
+// console.log(otherNumeber.toPrecision(3));
+
+// // use toPrecison carefully
+// const otherNumeberOne = 1123.8999
+// console.log(otherNumeberOne.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());            //this is according to USA sysytem
+// console.log(hundreds.toLocaleString('en-IN'));            //this is according to USA number sysytem
+
+
+// ************************MATHS********************************
+// console.log(Math);
+
+// console.log(Math.abs(-4))               //abs - absolute value , which converts the -ve value to +ve value
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(81))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
+
