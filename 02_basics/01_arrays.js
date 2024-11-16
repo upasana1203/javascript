0d213e9240759f91a774e95e195708ff4c50eafa
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2024-11-16 19:27:52 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+// array
+
+const array = [0,11,25,43,84]
+console.log(array[1]);
+
+const myArr2 = new Array(1,2,34,5,89)
+
+// array methods
+array.push(56)
+console.log(array)
+array.pop()
+
+array.unshift(9)
+console.log(array)
