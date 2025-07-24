3f955f2626d538b62700cfc3a995d324a28c791b
Upasana Sharma <141768601+upasana1203@users.noreply.github.com>
2025-07-24 19:21:27 +0000

    array methods more

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -22,8 +22,10 @@ const dc_heros = ["shaktiman" , "flash", "batman"]
 // console.log(Array.from({name:"Hitesh"}))         //this will not make array an array , but it will give you an empty array , becz when we give it keys and value so we have to tell it to make array either from keys or values , ----> it's a interesting case can ask in interviews
 
 
+
+
 let score1 = 100
 let score2 = 300
 let score3 = 500
 
-console.log(Array.of(score1,score2,score3))          // of -> returns a new array from a set of elemnts
+console.log(Array.of(score1,score2,score3))          // of -> returns a new array from a set of elemnts  
